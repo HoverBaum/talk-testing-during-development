1c1b2cdaed9a5cb224c5fe96ca649014c11ed019
Hendrik Wallbaum <hendrik.wallbaum@cubic.com>
2019-03-18 17:05:43 +0100

    :pen: move timeline component into talk.

---
+++ talk/components/timeline/typeMap.js
@@ -0,0 +1,34 @@
+const typeMap = [
+  {
+    type: 'release',
+    color: '#253F5A',
+    emojiImg: 'releasing-rocket.png'
+  },
+  {
+    type: 'meeting',
+    color: '#C48181',
+    emojiImg: 'meetings-calendar.png'
+  },
+  {
+    type: 'development',
+    color: '#F4DE78',
+    emojiImg: 'working-2-construction.png'
+  },
+  {
+    type: 'freeze',
+    color: '#9FE0FA',
+    emojiImg: 'codefreeze-snowflake.png'
+  },
+  {
+    type: 'testing',
+    color: '#a8792a',
+    emojiImg: 'testing-detective.png'
+  },
+  {
+    type: 'perfect',
+    color: '#F9DE47',
+    emojiImg: 'perfect-day.png'
+  }
+]
+
+export default typeMap
