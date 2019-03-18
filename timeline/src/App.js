39e39a561c45df1f6c42001f9ef6399bafdacd23
Hendrik Wallbaum <hendrik.wallbaum@cubic.com>
2019-03-18 16:25:09 +0100

    :sparkles: use React component in talk.
    Still feels quite hacky with having the images in multiple places and such.

--- timeline/src/App.js
+++ timeline/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Timeline from './timeline';
+
+import scenarioOne from './data/scenarioOne'
+import scenarioTwo from './data/scenarioTwo'
+import perfectData from './data/perfectData'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Timeline data={scenarioOne}></Timeline>
+        <Timeline data={scenarioTwo}></Timeline>
+        <Timeline data={perfectData}></Timeline>
       </div>
     );
   }
---
+++ timeline/src/timeline.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import typeMap from './data/typeMap'
+
+import './timeline.css'
+
+const fillDays = (days, types) => days.map(day => {
+  const parts = day.types.map(dayType => types.find(type => type.type === dayType))
+  return {
+    ...day,
+    parts
+  }
+})
+
+// Find fridays based on the index. Assuming we start at a Monday.
+const isFriday = day => day !== 0 && (day + 1) % 5 === 0
+
+// No need to render weekends for the last day.
+const shouldRenderWeekend = (currentDay, allDays) => isFriday(currentDay) && currentDay < allDays - 1
+
+const Timeline = ({data = []}) => (
+  <div className="timeline">
+    {fillDays(data, typeMap).map((day, index) => (
+      <>
+        <div className={`timeline__day ${isFriday(index) ? 'timeline__day_friday' : ''}`}>
+          {day.parts.map(part => (
+            <div className="timeline__part" style={{backgroundColor: part.color}}>
+              <img src={`images/${part.emojiImg}`} alt={part.emojiImg.replace(/\..*$/, '')} />
+            </div>
+          ))}
+        </div>
+        {shouldRenderWeekend(index, data.length) ? (
+          <div class="timeline__weekend"></div>
+        ) : ''}
+      </>
+    ))}
+  </div>
+)
+
+export default Timeline
