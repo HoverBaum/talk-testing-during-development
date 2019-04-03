f48f1cea527e963d5d3657e7e03778a188dd5c32
Hendrik Wallbaum <hendrik.wallbaum@cubic.com>
2019-04-03 13:53:08 +0200

    :sparkles: custom provider for slider numbers.

--- components/timeline/timeline.js
+++ components/timeline/timeline.js
@@ -14,6 +14,8 @@ const fillDays = (days, types) => days.map(day => {
 // Find fridays based on the index. Assuming we start at a Monday.
 const isFriday = day => day !== 0 && (day + 1) % 5 === 0
 
+const isLastDay = (day, total) => day === (total - 1)
+
 // No need to render weekends for the last day.
 const shouldRenderWeekend = (currentDay, allDays) => isFriday(currentDay) && currentDay < allDays - 1
 
@@ -21,7 +23,7 @@ const Timeline = ({data = []}) => (
   <div className="timeline">
     {fillDays(data, typeMap).map((day, index) => (
       <React.Fragment key={index}>
-        <div className={`timeline__day ${isFriday(index) ? 'timeline__day_friday' : ''}`}>
+        <div className={`timeline__day ${isFriday(index) ? 'timeline__day_friday' : ''} ${isLastDay(index, data.length) ? 'timeline__day_last' : ''}`}>
           {day.parts.map((part, key) => (
             <div key={key} className="timeline__part" style={{backgroundColor: part.color}}>
               <img src={`images/${part.emojiImg}`} alt={part.emojiImg.replace(/\..*$/, '')} />
