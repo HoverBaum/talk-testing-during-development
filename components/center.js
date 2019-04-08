dccca3b6037293e586fb3b284fbaedc05028965b
Hendrik Wallbaum <hendrik.wallbaum@cubic.com>
2019-04-08 13:56:04 +0200

    Add a centering component.

---
+++ components/center.js
@@ -0,0 +1,15 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
+export const Center = ({children}) => (
+  <div
+    css={css`     
+      display: flex;
+      justify-content: center;
+    `}
+  >
+    {children}
+  </div>
+)
+
+export default Center
