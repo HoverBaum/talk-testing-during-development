b62d34f664370f9431f09726fad41ea627d93d71
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2019-05-22 23:43:57 +0200

    :fire: remove last full screen image.

--- components/impact.js
+++ components/impact.js
@@ -2,13 +2,12 @@ import React from 'react'
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 
-const Impact = ({children, size = 3, onImage }) => (
-  <div 
+const Impact = ({ children, size = 3, onImage }) => (
+  <div
     css={css`
       font-size: ${size}em;
       font-weight: normal;
-      color: ${onImage ? 'white' : 'inherit'};
-      position: absolute;
+      color: ${onImage ? 'white' : '#334a5e'};
       left: 50%;
       top: 50%;
       transform: translateX(-50%) translateY(-50%);
