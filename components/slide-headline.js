a97decadd2d4447fe7e221d696eb8da2c5d94c36
Hendrik Wallbaum <hendrik.wallbaum@cubic.com>
2019-05-22 17:14:26 +0200

    :sparkles: introuce headlines for slides.

---
+++ components/slide-headline.js
@@ -0,0 +1,31 @@
+import React from 'react'
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
+const SlideHeadline = ({ children, sectionTitle }) => (
+  <h2
+    css={{
+      position: 'absolute',
+      top: 0,
+      borderBottom: '1px solid #334a5e',
+      width: '80vw',
+      fontWeight: 300,
+      marginTop: '0.25em',
+      color: '#334a5e',
+    }}
+  >
+    {sectionTitle && (
+      <>
+      <span
+        css={{
+          color: '#cc4a1a'
+        }}
+      >{sectionTitle}</span>
+      {children && <span> - </span>}
+      </>
+    )}
+    {children}
+  </h2>
+)
+
+export default SlideHeadline
