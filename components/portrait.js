e62db4ffb60b197dfdaefcda2d417be520f62e92
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2019-05-22 21:34:42 +0200

    :lipstick: update portrait shadow.
    Use the same as for the demo page.

--- components/portrait.js
+++ components/portrait.js
@@ -9,7 +9,8 @@ export const Portrait = ({src}) => (
       width: dimension,
       height: dimension,
       maxWidth: '100%',
-      boxShadow: '0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12)'
+      boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+      borderRadius: '0.5rem 0.5rem 0 0'
     }}
     src={src}
   />
