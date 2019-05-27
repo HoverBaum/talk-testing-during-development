e76adccbaa7e572cb5a95cac00acaa011c0884ee
Hendrik <mail@hendrikwallbaum.de>
2019-05-27 17:15:46 +0200

    :sparkles: add conclusion slide.

--- components/opening-slide.js
+++ components/opening-slide.js
@@ -2,7 +2,7 @@ import React from 'react'
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 
-const OpeningSlide = ({ title, author }) => (
+const OpeningSlide = ({ title, author, subtitle }) => (
   <hgroup
     css={{
       width: '70vw'
@@ -19,13 +19,26 @@ const OpeningSlide = ({ title, author }) => (
     >
       {title}
     </h1>
+    {subtitle && (
+      <h4
+        css={{
+          marginTop: '2rem',
+          color: '#cc4a1a'
+        }}
+      >
+        { subtitle }
+      </h4>
+    )}
     {author && (
       <a href='https://hendrikwallbaum.de' target='_blank'>
         <h4
           css={{
-            float: 'right',
+            position: 'absolute',
+            bottom: 0,
+            width: '80vw',
             marginTop: '2rem',
-            color: '#cc4a1a'
+            color: '#cc4a1a',
+            textAlign: 'right'
           }}
         >
           by {author}
