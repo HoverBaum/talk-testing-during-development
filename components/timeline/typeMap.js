9cab3fa4195109cda01037c20060c87b970b7be4
Hendrik <mail@hendrikwallbaum.de>
2019-10-24 07:39:41 +0200

    :checkered_flag: final adjustments for code.talks.

--- components/timeline/typeMap.js
+++ components/timeline/typeMap.js
@@ -13,27 +13,27 @@ const typeMap = [
   },
   {
     type: 'meeting',
-    color: '#C1666B', //'#d37883',
+    color: '#C16E72', //'#d37883',
     emojiImg: MeetingImage,
   },
   {
     type: 'development',
-    color: '#479e83', //'#FFE0B5',
+    color: '#EFC2C4', //'#479e83', //'#FFE0B5',
     emojiImg: WorkingImage,
   },
   {
     type: 'freeze',
-    color: '#D5DFE5', //'#a8e4f7',
+    color: '#CCD8E5', //'#a8e4f7',
     emojiImg: CodeFreezeImage,
   },
   {
     type: 'testing',
-    color: '#D5DFE5', //'#933636',
+    color: '#CCD8E5', //'#933636',
     emojiImg: TestingImage,
   },
   {
     type: 'perfect',
-    color: '#FFD670',
+    color: '#FFDD87', // '#FFD670',
     emojiImg: PerfectDayImage,
   },
 ]
